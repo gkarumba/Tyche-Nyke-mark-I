9bec92f39208c1bd324cbf02d224aa722a5682b3
gkarumba <gachegua@gmail.com>
2018-12-21 15:21:59 +0300

    [finishes #162805083] Adds the database.py file

---
+++ app/database/database.py
@@ -0,0 +1,109 @@
+import os 
+import psycopg2
+from psycopg2.extras import RealDictCursor
+#local import 
+from instance.config import config
+
+env = os.getenv('FLASK_ENV')
+url = config[env].DATABASE_URL
+
+class BooksDB():
+    """
+    Class for methods to handle databse queries 
+    """
+    def __init__(self):
+        """
+        Method for initializing the class BooksDB
+        """
+        self.conn = psycopg2.connect(url)
+        self.cur = self.conn.cursor(cursor_factory=RealDictCursor)
+
+    def create_tables(self):
+        """
+        Method for creating tables in the database
+        """
+        queries = (
+            """CREATE TABLES IF NOT EXISTS books(
+                id SERIAL PRIMARY KEY,
+                book_name varchar(42) NOT NULL,
+                author varchar(42) NOT NULL,
+                category varchar(42) NOT NULL,
+                status varchar(42) default 'Available'
+                )""",
+            """CREATE TABLES IF NOT EXISTS users(
+                id SERIAL PRIMARY KEY,
+                username varchar(42) NOT NULL,
+                email varchar(42) NOT NULL,
+                role varchar(42) NOT NULL,
+                password varchar(42) NOT NULL,
+                books_borrowed INT REFERENCES books(id)
+            )""";
+        )
+        for q in queries:
+            self.cur.execute(q)
+            self.conn.commit()
+
+    def drop_tables(self):
+        """
+        Method for dropping the tables after running tests
+        """
+        queries = (
+            """DROP TABLE IF EXISTS books CASCADE""";,
+            """DROP TABLES IF EXISTS users CASACADE""";
+        )
+        for q in queries:
+            self.cur.execute(q)
+            self.conn.commit()
+
+    def add_to_db(self,query,tuple_data):
+        """
+        Method for adding to the database
+        """
+        self.cur.execute(query,tuple_data)
+        self.conn.commit()
+        response = self.cur.fetchone()
+        return response
+
+    def get_all(self,query):
+        """
+        Method for retrieving all the books
+        """
+        self.cur.execute(query)
+        return self.fetchall()
+    
+    def get_one(self,query):
+        """
+        Method for retrieving a single book
+        """
+        self.cur.execute(query)
+        return self.fetchone()
+
+    def edit_book(self,query):
+        """
+        Method for editing a book
+        """
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def borrow_book(self,query):
+        """
+        Method for borrowing a book
+        """
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def return_book(self,query):
+        """
+        Method for returning a book
+        """
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def unreturned_book(self,query):
+        """
+        Method for retrieving unreturned books
+        """
+        self.cur.execute(query)
+        return self.fetchall()
+
+    
--- instance/config.py
+++ instance/config.py
@@ -12,13 +12,15 @@ class DevelopmentConfig(Config):
         Class for the development configuration
     """
     DEBUG  = True
+    DATABASE_URL = os.getenv('DATABASE_URL')
 
 class TestingConfig(Config):
-    DEBUG = True
-    TEST = True
     """
         Class for the testing configuration
     """
+    DEBUG = True
+    TEST = True
+    DATABASE_URL = os.getenv('TEST_DB_URL')
 
 config = {
     "development_config" : DevelopmentConfig,
