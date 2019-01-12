7433a8cf68e689103d52d1c09504227045b40f37
gkarumba <gachegua@gmail.com>
2019-01-12 13:41:15 +0300

    [finishes #162805042] Adds the edit book_name,author and category

--- app/api/books/v2/__init__.py
+++ app/api/books/v2/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 from flask_restful import Resource,Api
 #local imports
-from app.api.books.v2.views import AddBooks,GetBook
+from app.api.books.v2.views import AddBooks,GetBook,EditTitle,EditAuthor,\
+                                   EditCategory
 
 library_v2 = Blueprint('lbv2',__name__,url_prefix='/api/v2')
 
@@ -9,3 +10,6 @@ api = Api(library_v2)
 
 api.add_resource(AddBooks,'/books')
 api.add_resource(GetBook,'/books/<int:id>')
+api.add_resource(EditTitle,'/books/title/<int:id>')
+api.add_resource(EditAuthor,'/books/author/<int:id>')
+api.add_resource(EditCategory,'/books/category/<int:id>')
--- app/api/books/v2/models.py
+++ app/api/books/v2/models.py
@@ -19,27 +19,18 @@ class BooksModel():
         """
         Method of adding a book
         """
-        payload = {
-            "book_name":book_name,
-            "author":author,
-            "category":category
-        }
         create_db = db.create_tables()
         query = """INSERT INTO books(book_name,author,category) VALUES (%s,%s,%s);"""
         tuple_data = (book_name,author,category)
         response = db.add_book(query,tuple_data)
         query2 = """SELECT id FROM books WHERE id = (select max(id) from books);"""
         result = db.get_one(query2)
-        # print(result)
         return result
 
     def check_book(self,title):
         """
         Method for checking if a book already exists
         """
-        payload = {
-            'title':title
-        }
         query = """SELECT book_name FROM books WHERE book_name='{}'""".format(title)
         response = db.get_one(query)
         # print(response)
@@ -61,4 +52,46 @@ class BooksModel():
         """
         query = """ SELECT * FROM books WHERE id = '{}'""".format(id)
         response = db.get_one(query)
+        return response
+    
+    def edit_book_title(self,title,id):
+        """
+        Method for editing the book title
+        """
+        check_query = """ SELECT book_name FROM books WHERE id = '{}'""".format(id)
+        check_response = db.get_one(check_query)
+        if not check_response:
+            return False
+        query = """UPDATE books SET book_name = '{}' WHERE id = '{}'""".format(title,id)
+        db.edit_book(query)
+        get_query = """  SELECT * FROM books WHERE id = '{}'""".format(id)
+        response = db.get_one(get_query)
+        return response
+
+    def edit_book_author(self,author,id):
+        """
+        Method for editing the book author
+        """
+        check_query = """ SELECT author FROM books WHERE id = '{}'""".format(id)
+        check_response = db.get_one(check_query)
+        if not check_response:
+            return False
+        query = """UPDATE books SET author = '{}' WHERE id = '{}'""".format(author,id)
+        db.edit_book(query)
+        get_query = """  SELECT * FROM books WHERE id = '{}'""".format(id)
+        response = db.get_one(get_query)
+        return response
+
+    def edit_book_category(self,category,id):
+        """
+        Method for editing the book category
+        """
+        check_query = """ SELECT category FROM books WHERE id = '{}'""".format(id)
+        check_response = db.get_one(check_query)
+        if not check_response:
+            return False
+        query = """UPDATE books SET category = '{}' WHERE id = '{}'""".format(category,id)
+        db.edit_book(query)
+        get_query = """  SELECT * FROM books WHERE id = '{}'""".format(id)
+        response = db.get_one(get_query)
         return response
