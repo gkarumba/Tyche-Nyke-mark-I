35a60aaad33561d3bf093737e6eb0a0d1521ebb9
gkarumba <gachegua@gmail.com>
2019-01-21 18:38:15 +0300

    [finishes #162805052] Adds the get user borrowing history endpoint

--- app/api/users/v2/__init__.py
+++ app/api/users/v2/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 from flask_restful import Resource,Api
 #local imports
-from app.api.users.v2.views import Register,Login,GetUnreturned
+from app.api.users.v2.views import Register,Login,GetUnreturned,\
+                                    GetBorrowHistory
 
 users_v2 = Blueprint('user_v2',__name__,url_prefix='/api/v2')
 
@@ -10,3 +11,4 @@ api = Api(users_v2)
 api.add_resource(Register,'/users/register')
 api.add_resource(Login,'/users/login')
 api.add_resource(GetUnreturned,'/users/books/unreturned')
+api.add_resource(GetBorrowHistory,'/users/books/<int:id>')
