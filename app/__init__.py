c7fcca229f46e4e8671f8167f2f7da6da7ad9199
gkarumba <gachegua@gmail.com>
2019-02-14 15:33:06 +0300

    Adds the create users table

--- app/__init__.py
+++ app/__init__.py
@@ -15,9 +15,8 @@ def create_app(config_name="development_config"):
     """
     app = Flask(__name__,instance_relative_config=True)
     app.config.from_object(config.config[config_name])
-    # db.start_db(app.config['DATABASE_URL'])
     db.create_tables()
-    db.create_users_table()
+    # db.create_users_table()
     db.create_borrow_table()
     app.register_blueprint(library_v1)
     app.register_blueprint(library_v2)
